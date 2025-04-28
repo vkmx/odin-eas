1ee7f8c074fbd7d4e0b0e75d52513204692c86de
vkmx <vkmx@users.noreply.github.com>
2025-04-28 11:15:57 +0530

    Add basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-asdasda
+let gridLength = 16;
+let gridSize = gridLength * gridLength;
+let container = document.querySelector('.grid');
+let gridItems = document.createDocumentFragment();
+let itemSize = `${100/gridLength}%`;
+
+for( let i = 0; i < gridSize; i++ ){
+    let item = document.createElement('div');
+    item.className = 'grid-item';
+    item.style.minWidth = itemSize;
+    item.style.minHeight = itemSize;
+
+    gridItems.appendChild( item );
+}
+
+container.appendChild( gridItems );
+
+container.addEventListener( 'mouseover', ( event ) => {
+    const target = event.target;
+
+    target.style.backgroundColor = 'black';
+});
